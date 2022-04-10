0ab8918258084097b57168ecc6ee83b310bc6f6c
headlesscrown <mdh8533@nyu.edu>
2022-04-10 15:31:46 -0400

    script tweak

--- teamCoding/Assets/Scripts/ScreenBoundaries.cs
+++ teamCoding/Assets/Scripts/ScreenBoundaries.cs
@@ -12,16 +12,16 @@ public class ScreenBoundaries : MonoBehaviour
     void Start()
     {
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)); //set boundaries
-        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2; //obj width
-        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2; //obj height
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x; //obj width
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y; //obj height
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         Vector3 viewPos = transform.position; //set position
-        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x + objectWidth, screenBounds.x * -1 - objectWidth); //clamp x boundary
-        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y + objectHeight, screenBounds.y * -1 - objectHeight); //clamp y boundary
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x, screenBounds.x * -1); //clamp x boundary
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y, screenBounds.y * -1); //clamp y boundary
         transform.position = viewPos; //set to new position
     }
 }
